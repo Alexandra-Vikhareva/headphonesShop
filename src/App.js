8f8aa6e6ec07f6cd9bd8cf5aaf89d45cf25fd5b5
Sasha <st087955@student.spbu.ru>
2024-09-22 23:01:41 +0300

    reset styles, made the card with css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
+import "./styles/App.css";
+import im from './images/airpods.jpg';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="card">
+        <div className="card_content">
+          <img src={im} alt="headphones image" height='240px' width='210px'/>
+          <div className="info">
+            <div className="description">
+              <h3 className="title">Apple BYZ S852I</h3>
+              <p className="rate">4.7</p>
+            </div>
+            <div className="value_container">
+              <p className="price">2927 Р</p>
+              <button className="buy">Купить</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
